69daef3ab2217a337080527460dba00ff4806686
prshawkat <showkatalimon@gmail.com>
2020-06-27 11:53:51 +0600

    project completed

--- resources/views/backend/footer.blade.php
+++ resources/views/backend/footer.blade.php
@@ -1,7 +1,7 @@
 
 <!-- /.content-wrapper -->
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2020 <a target="_blank" href="https://www.shawkatali.com/">Shawkat Ali</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 3.0.3-pre
